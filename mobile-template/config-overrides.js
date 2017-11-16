be6aa90ebd21b608a8c8437a670a024f1be65fd4
HuangLi <huangliop@163.com>
2017-11-16 14:25:55 +0800

    更新react版本

--- mobile-template/config-overrides.js
+++ mobile-template/config-overrides.js
@@ -5,5 +5,7 @@ module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
     //decorator
     config=injectBabelPlugin('transform-decorators-legacy',config);
+    //正式环境下用cheap-module-source-map
+    if(env === "production")config.devtool='cheap-module-source-map';
     return config;
   };
